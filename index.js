0916cfe8fddc640d703e862d9d838e3240b4ac5a
anas <anasstudy01@gmail.com>
2025-04-28 00:38:51 +0530

    feat: add axios and axis dependencies; update node-telegram-bot-api version chore: create .env file for bot token configuration

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+const TelegramBot = require('node-telegram-bot-api');
 const dotenv  = require('dotenv');
+const axios = require('axios');
+
+
+
 dotenv.config();
+
 console.log(process.env.Bot_TOKEN);
+
+
+
+
+const token = process.env.Bot_TOKEN;
+
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot  = new TelegramBot(token, { polling: true });
+
+bot.on('message',(msg)=>{const text = msg.text;
+    console.log('message recieverd :',text);
+
+    bot.sendMessage(msg.chat.id,`Hello ${msg.from.first_name}, you sent me: ${text}`);
+})
+
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Welcome to the bot! How can I assist you today?');
+});
+
+
+bot.onText(/\/jokes/,async(msg) => {
+    const chatId = msg.chat.id;
+    try {
+        const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
+        const joke = `${response.data.setup} - ${response.data.punchline}`;
+        bot.sendMessage(chatId, joke);
+    } catch (error) {
+        console.error('Error fetching joke:', error);
+        bot.sendMessage(chatId, 'Sorry, I could not fetch a joke at the moment.');
+    }
+})
